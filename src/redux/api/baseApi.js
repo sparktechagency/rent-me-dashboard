c72eea58e77ebd3650de2b074661081e9418e4da
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-03-11 11:05:32 +0600

    updated

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -5,9 +5,8 @@ import Cookies from "js-cookie";
 // Enhanced base query to handle token refresh
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   const baseQuery = fetchBaseQuery({
-    // baseUrl: "http://139.59.0.25:5098/api/v1",
-    // baseUrl: "http://10.0.80.49:5030/api/v1",
-    baseUrl: "https://asad.binarybards.online/api/v1",
+    // baseUrl: "https://asad.binarybards.online/api/v1",
+    baseUrl: "http://54.241.243.33:5000/api/v1",
     prepareHeaders: (headers) => {
       const token =
         localStorage.getItem("authToken") ||
@@ -84,6 +83,6 @@ export const api = createApi({
 });
 
 // Export the image URL as a constant
-// export const imageUrl = "http://139.59.0.25:5098";
-// export const imageUrl = "http://10.0.80.49:5030";
-export const imageUrl = "https://asad.binarybards.online";
+
+export const imageUrl = "http://54.241.243.33:5000";
+// export const imageUrl = "https://asad.binarybards.online";
